47ad9404b78cef604e8a46f3a59372bc21741ab6
naelloay363-gif <naelloay363@gmail.com>
2025-12-20 14:59:02 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+/* Rawana Shoes - Offline SW */
+const CACHE_NAME = "rawana-shoes-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+
+  // صفحات الـ HTML: جرّب الشبكة أولاً ثم الكاش (عشان التحديثات)
+  if (req.mode === "navigate") {
+    event.respondWith(
+      fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put("./index.html", copy));
+        return res;
+      }).catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  // باقي الملفات: كاش أولاً
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req))
+  );
+});
